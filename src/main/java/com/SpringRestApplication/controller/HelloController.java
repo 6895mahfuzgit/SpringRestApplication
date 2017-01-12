b7b6869934a58e99deef0c7731cb9365af97e911
6895mahfuzgit <6895mahfuz@gmail.com>
2017-01-12 20:01:57 +0600

    Added hello Controller

---
+++ src/main/java/com/SpringRestApplication/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.SpringRestApplication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by user on 12-Jan-17.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHi() {
+
+        return "Hi";
+    }
+}
